4938760126443420e099ab8679a23623d6c14c25
Chunan Ye <chuye@microsoft.com>
2023-07-31 11:48:15 -0700

    Move "delete database credential" to server node Mock AAD credential picker

--- src/postgres/commands/deletePostgresDatabaseConnection.ts
+++ src/postgres/commands/deletePostgresDatabaseConnection.ts
@@ -8,23 +8,23 @@ import * as vscode from 'vscode';
 import { postgresFlexibleFilter, postgresSingleFilter } from '../../constants';
 import { ext } from "../../extensionVariables";
 import { localize } from "../../utils/localize";
-import { PostgresDatabaseTreeItem } from "../tree/PostgresDatabaseTreeItem";
+import { PostgresServerTreeItem } from "../tree/PostgresServerTreeItem";
 
-export async function deletePostgresDatabaseCredentials(context: IActionContext, node?: PostgresDatabaseTreeItem): Promise<void> {
+export async function deletePostgresDatabaseCredentials(context: IActionContext, node?: PostgresServerTreeItem): Promise<void> {
     const suppressCreateContext: ITreeItemPickerContext = context;
     suppressCreateContext.suppressCreatePick = true;
     if (!node) {
-        node = await ext.rgApi.pickAppResource<PostgresDatabaseTreeItem>(context, {
+        node = await ext.rgApi.pickAppResource<PostgresServerTreeItem>(context, {
             filter: [postgresSingleFilter, postgresFlexibleFilter],
-            expectedChildContextValue: PostgresDatabaseTreeItem.contextValue
+            expectedChildContextValue: PostgresServerTreeItem.contextValue
         });
     }
 
-    const message: string = localize('deletesPostgresDatabaseCredentials', 'Are you sure you want to delete credentials for database "{0}"?', node.databaseName);
+    const message: string = localize('deletesPostgresDatabaseCredentials', 'Are you sure you want to delete credentials for database server "{0}"?', node.azureName);
     const result = await context.ui.showWarningMessage(message, { modal: true, stepName: 'deletePostgresDatabaseConnection' }, DialogResponses.deleteResponse);
     if (result === DialogResponses.deleteResponse) {
-        await node.parent.deletePostgresCredentials();
-        const deleteMessage: string = localize('deletePostgresDatabaseConnectionMsg', 'Successfully deleted credentials for database "{0}".', node.databaseName);
+        await node.deletePostgresCredentials();
+        const deleteMessage: string = localize('deletePostgresDatabaseConnectionMsg', 'Successfully deleted credentials for database server "{0}".', node.azureName);
         void vscode.window.showInformationMessage(deleteMessage);
         ext.outputChannel.appendLog(deleteMessage);
     }
--- src/postgres/commands/enterPostgresCredentials.ts
+++ src/postgres/commands/enterPostgresCredentials.ts
@@ -20,44 +20,64 @@ export async function enterPostgresCredentials(context: IActionContext, treeItem
         });
     }
 
-    let username: string = await context.ui.showInputBox({
-        prompt: localize('enterUsername', 'Enter username for server "{0}"', treeItem.label),
-        stepName: 'enterPostgresUsername',
-        validateInput: (value: string) => { return (value && value.length) ? undefined : localize('usernameCannotBeEmpty', 'Username cannot be empty.'); }
+    const aadCredentialOption = { label: 'Azure AD credential', description: 'Recommended', detail: `Use the signed-in Azure account's Azure AD credential`, picked: true };
+    const passwordCredentialOption = { label: 'Username and password', detail: 'Use database server user name and password' };
+    let credentialType = await context.ui.showQuickPick([
+        aadCredentialOption,
+        passwordCredentialOption
+    ], {
+        title: localize(`Select credential type to use for server "{0}"`, treeItem.label),
+        learnMoreLink: "https://aka.ms"
     });
 
-    const serverName: string = nonNullProp(treeItem, 'azureName');
-    // Username doesn't contain servername prefix for Postgres Flexible Servers only
-    // As present on the portal for any Flexbile Server instance
-    const usernameSuffix: string = `@${serverName}`;
-    if (treeItem.serverType === PostgresServerType.Single && !username.includes(usernameSuffix)) {
-        username += usernameSuffix;
+    if (credentialType === aadCredentialOption) {
+        // @todo: Support connect using AAD credential
+    } else {
+        let username: string = await context.ui.showInputBox({
+            prompt: localize('enterUsername', 'Enter username for server "{0}"', treeItem.label),
+            stepName: 'enterPostgresUsername',
+            validateInput: (value: string) => { return (value && value.length) ? undefined : localize('usernameCannotBeEmpty', 'Username cannot be empty.'); }
+        });
+
+        const serverName: string = nonNullProp(treeItem, 'azureName');
+        // Username doesn't contain servername prefix for Postgres Flexible Servers only
+        // As present on the portal for any Flexbile Server instance
+        const usernameSuffix: string = `@${serverName}`;
+        if (treeItem.serverType === PostgresServerType.Single && !username.includes(usernameSuffix)) {
+            username += usernameSuffix;
+        }
+
+        const password: string = await context.ui.showInputBox({
+            prompt: localize('enterPassword', 'Enter password for server "{0}"', treeItem.label),
+            stepName: 'enterPostgresPassword',
+            password: true,
+            validateInput: (value: string) => { return (value && value.length) ? undefined : localize('passwordCannotBeEmpty', 'Password cannot be empty.'); }
+        });
+
+        const id: string = nonNullProp(treeItem, 'id');
+
+        const progressMessage: string = localize('setupCredentialsMessage', 'Setting up credentials for server "{0}"...', serverName);
+        const options: vscode.ProgressOptions = {
+            location: vscode.ProgressLocation.Notification,
+            title: progressMessage
+        };
+
+        await vscode.window.withProgress(options, async () => {
+            await setPostgresCredentials(username, password, id);
+            // @todo: test, get feedback
+            vscode.window.showWarningMessage(`You are using username and password to connect to your Postgres database. It's recommended to use passwordless connections for Azure-based services.`, 'Documentation').then((value) => {
+                if (value === 'Documentation') {
+                    vscode.env.openExternal(vscode.Uri.parse('https://learn.microsoft.com/azure/developer/intro/passwordless-overview'));
+                }
+            });
+        });
+
+        const completedMessage: string = localize('setupCredentialsMessage', 'Successfully added credentials to server "{0}".', serverName);
+        void vscode.window.showInformationMessage(completedMessage);
+        ext.outputChannel.appendLog(completedMessage);
+
+        treeItem.setCredentials(username, password);
+
+        await treeItem.refresh(context);
     }
-
-    const password: string = await context.ui.showInputBox({
-        prompt: localize('enterPassword', 'Enter password for server "{0}"', treeItem.label),
-        stepName: 'enterPostgresPassword',
-        password: true,
-        validateInput: (value: string) => { return (value && value.length) ? undefined : localize('passwordCannotBeEmpty', 'Password cannot be empty.'); }
-    });
-
-    const id: string = nonNullProp(treeItem, 'id');
-
-    const progressMessage: string = localize('setupCredentialsMessage', 'Setting up credentials for server "{0}"...', serverName);
-    const options: vscode.ProgressOptions = {
-        location: vscode.ProgressLocation.Notification,
-        title: progressMessage
-    };
-
-    await vscode.window.withProgress(options, async () => {
-        await setPostgresCredentials(username, password, id);
-    });
-
-    const completedMessage: string = localize('setupCredentialsMessage', 'Successfully added credentials to server "{0}".', serverName);
-    void vscode.window.showInformationMessage(completedMessage);
-    ext.outputChannel.appendLog(completedMessage);
-
-    treeItem.setCredentials(username, password);
-
-    await treeItem.refresh(context);
 }
