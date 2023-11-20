72af45cab144c27f04c8c63a50ceefcdc9d51d71
Chunan Ye <chuye@microsoft.com>
2023-11-20 13:23:52 -0800

    Enable OAuth for CosmosDB resources

--- src/extensionVariables.ts
+++ src/extensionVariables.ts
@@ -43,6 +43,7 @@ export namespace ext {
         export const enableEndpointDiscovery = 'cosmosDB.enableEndpointDiscovery';
         export const mongoShellTimeout = 'mongo.shell.timeout';
         export const batchSize = 'azureDatabases.batchSize';
+        export const azureCliPath = 'azureDatabases.azureCliPath';
 
         export namespace vsCode {
             export const proxyStrictSSL = "http.proxyStrictSSL";
